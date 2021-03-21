8e5a37c799a165f3e4925671be3463a830656c71
akomissarova <akomissarovaaaa@gmail.com>
2021-03-21 21:57:42 +0100

    Added docstrings to helper classes

--- helpers/app.py
+++ helpers/app.py
@@ -2,8 +2,24 @@ from helpers.leads import LeadsHelper
 
 
 class Application:
+    """
+    Basic fixture class
+
+    Initializes variables needed to connect to REST API and provides access to helper classes
+    (at the moment - only one class) to the tests
+    """
 
     def __init__(self, baseurl, api_version, api_token):
+        """
+        Parameters
+        ----------
+        baseurl : str
+            Sell API URL
+        api_version : str
+            API version prefix
+        api_token : str
+            API access token
+        """
         self.url = baseurl+api_version
         self.basic_headers = {'Accept': 'application/json',
                               'Authorization': 'Bearer {}'.format(api_token)}
--- helpers/leads.py
+++ helpers/leads.py
@@ -2,14 +2,50 @@ import requests
 
 
 class LeadsHelper:
+    """
+    A class providing methods to operate leads via REST API
+
+    Attributes
+    ----------
+    RESOURCE : str
+        resource for operations on leads
+
+    Methods
+    -------
+    create(lead_data)
+        Creates a lead and returns its parameters if the server returned 200
+    get_lead(lead_id)
+        Gets a lead by id and returns its parameters if the lead exists on a server
+    exists(lead_id)
+        Returns True if the lead with a given id exists on a server
+    modify(lead_id, lead_data)
+        Modifies lead with the given id and returns its parameters if the server returned 200
+    """
 
     RESOURCE = '/leads'
 
     def __init__(self, app):
+        """
+        Parameters
+        ----------
+        app : Application
+            The Application object with necessary setting to use REST API
+        """
         self.app = app
         self.url = app.url + self.RESOURCE
 
     def create(self, lead_data):
+        """
+        Parameters
+        ----------
+        lead_data : dict
+            Parameters of the lead to be created
+
+        Returns
+        _______
+        dict
+            JSON with created lead parameters if server returned 200, otherwise JSON with server errors
+        """
         headers = {**self.app.basic_headers, 'Content-Type': 'application/json'}
         r = requests.post(self.url, headers=headers, json=lead_data)
         if r.status_code == 200:
@@ -18,6 +54,17 @@ class LeadsHelper:
             return r.json()
 
     def get_lead(self, lead_id):
+        """
+        Parameters
+        ----------
+        lead_id : int
+            Lead ID
+
+        Returns
+        _______
+        dict
+            JSON with lead parameters if server returned 200, otherwise JSON with server errors
+        """
         r = requests.get('{url}/{lead_id}'.format(url=self.url, lead_id=str(lead_id)),
                          headers=self.app.basic_headers)
         if r.status_code == 200:
@@ -26,6 +73,17 @@ class LeadsHelper:
             return r.json()
 
     def exists(self, lead_id):
+        """
+        Parameters
+        ----------
+        lead_id : int
+            Lead ID
+
+        Returns
+        _______
+        bool
+            True if server returned 200, False if server returned 404 and None if server returned something else
+        """
         r = requests.get('{url}/{lead_id}'.format(url=self.url, lead_id=str(lead_id)),
                          headers=self.app.basic_headers)
         if r.status_code == 200:
@@ -36,6 +94,19 @@ class LeadsHelper:
             return None
 
     def modify(self, lead_id, lead_data):
+        """
+        Parameters
+        ----------
+        lead_id : int
+            Lead ID
+        lead_data : dict
+            Lead parameters to be modified
+
+        Returns
+        _______
+        dict
+            JSON with lead parameters if server returned 200, otherwise JSON with server errors
+        """
         headers = {**self.app.basic_headers, 'Content-Type': 'application/json'}
         r = requests.put('{url}/{lead_id}'.format(url=self.url, lead_id=str(lead_id)),
                          headers=headers, json={'data': lead_data})
