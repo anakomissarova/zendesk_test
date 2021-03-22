105c90c5ae344ffc68054adf9a2f3b295d493c68
akomissarova <akomissarovaaaa@gmail.com>
2021-03-22 11:48:27 +0100

    Renamed a variable in test_add_address_to_lead

--- tests/test_lead.py
+++ tests/test_lead.py
@@ -5,13 +5,13 @@ def test_add_address_to_lead(app):
     lead_data = {'data': {'last_name': 'Johnson', 'organization_name': str(datetime.now())}}
     lead_id = app.lead.create(lead_data)['id']
     assert app.lead.exists(lead_id)
-    address = {'address': {
+    address_data = {'address': {
         'line1': '2726 Smith Street',
         'city': 'Hyannis',
         'postal_code': '02601',
         'state': 'MA',
         'country': 'US'
     }}
-    app.lead.modify(lead_id, address)
+    app.lead.modify(lead_id, address_data)
     new_lead_data = app.lead.get_lead(lead_id)
-    assert address['address'] == new_lead_data['address']
+    assert address_data['address'] == new_lead_data['address']
